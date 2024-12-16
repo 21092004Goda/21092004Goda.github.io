12245c73d9817c1b2d42db283e9bb6cb7f3210b6
Kuro <samb21092004@gmail.com>
2024-12-16 18:07:38 +0300

    kjsdf

--- lab_7/scripts/SweetAlert2/start_info_window.js
+++ lab_7/scripts/SweetAlert2/start_info_window.js
@@ -1,19 +1,17 @@
 document.addEventListener('DOMContentLoaded', function() {
-    // Проверяем, показывалось ли окно ранее
-    if (!localStorage.getItem('profilePictureShown')) {
+    if (!sessionStorage.getItem('profilePictureShown')) {
         Swal.fire({
             title: "Profile Picture", // Заголовок окна
-            text: "Hey, I see you...", // Текст
-            imageUrl: "https://images3.imgbox.com/4f/e6/wOhuryw6_o.jpg", // Изображение
+            text: "Hey, I see you...", // Основной текст
+            imageUrl: "https://images3.imgbox.com/4f/e6/wOhuryw6_o.jpg", // Изображение для отображения
             imageWidth: 550, // Ширина изображения
             imageHeight: 225, // Высота изображения
-            imageAlt: "Eagle Image", // Альтернативный текст для изображения
-            confirmButtonText: "Who are you?", // Текст на кнопке
-            confirmButtonColor: "#00ff55",  // Цвет кнопки
-            showCancelButton: false,  // Убираем кнопку отмены
+            imageAlt: "Eagle Image", // Альтернативный текст для изображения, если оно не загрузится
+            confirmButtonText: "Who are you?", // Текст на кнопке подтверждения
+            confirmButtonColor: "#00ff55",  // Цвет кнопки подтверждения
+            showCancelButton: false,  // Отключаем кнопку отмены
         }).then(function() {
-            // После того, как окно будет закрыто, сохраняем флаг в localStorage
-            localStorage.setItem('profilePictureShown', 'true');
+            sessionStorage.setItem('profilePictureShown', 'true');
         });
     }
 });
